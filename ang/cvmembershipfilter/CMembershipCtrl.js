cf65bc743e60cda4e02b1c238a27309526b4a9f8
Md. Al-Amin opu <alamin.opu10@gmail.com>
2016-10-10 04:05:34 +0600

    Pretty basic date filter

--- ang/cvmembershipfilter/CMembershipCtrl.js
+++ ang/cvmembershipfilter/CMembershipCtrl.js
@@ -26,6 +26,10 @@
       'CvmembershipfilterCMembershipCtrl', 
       function($scope, crmApi, crmStatus, crmUiHelp, CurrentMemberships) {
 
+        $scope.dateFilter = {
+          startFrom: '',
+          startTo: ''
+        };
         $scope.memberships = CurrentMemberships.values;
         console.log($scope.memberships);
 
@@ -37,4 +41,27 @@
 
       }
     );
+
+  angular
+    .module('cvmembershipfilter')
+    .filter(
+      'StartRangeFilter', 
+      function(){
+        return function(data, dateFilter){
+          if(dateFilter.startFrom === '' || dateFilter.startTo === ''){
+            return data;
+          }
+
+          var filteredData = [];
+          for(var i=0; i<data.length; i++){
+            if(data[i].start_date >= dateFilter.startFrom
+              && data[i].start_date <= dateFilter.startTo){
+              filteredData.push(data[i]);
+            }
+          }
+          return filteredData;
+        }
+      }
+    );
+
 })(angular, CRM.$, CRM._);
