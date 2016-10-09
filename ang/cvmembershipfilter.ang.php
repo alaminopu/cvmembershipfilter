2062c00443f74e58e58f7e184bfe453534c87c79
Md. Al-Amin opu <alamin.opu10@gmail.com>
2016-10-09 23:21:10 +0600

    Add extention default files and changed name

---
+++ ang/cvmembershipfilter.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/cvmembershipfilter.js',
+    1 => 'ang/cvmembershipfilter/*.js',
+    2 => 'ang/cvmembershipfilter/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/cvmembershipfilter.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/cvmembershipfilter',
+  ),
+  'settings' => 
+  array (
+  ),
+);
